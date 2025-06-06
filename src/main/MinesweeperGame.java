3a7f8b1c2d7ecc3f231e4e656d0364f89cd15f93
BlazeHunterVN1610 <khanhdavidcr12345@gmail.com>
2025-06-06 13:46:20 +0700

    Update MinesweeperGame.java

--- src/main/MinesweeperGame.java
+++ src/main/MinesweeperGame.java
@@ -5,10 +5,8 @@ import java.awt.*;
 
 public class MinesweeperGame {
     public static void main(String[] args) throws Exception {
-        // Load the icon image (assuming it's located at "src/assets/textures/ms_logo.png")
-        Image icon = Toolkit.getDefaultToolkit().getImage("src/assets/textures/ms_logo.png");
 
-        // Set the icon before showing the menu
+        Image icon = Toolkit.getDefaultToolkit().getImage("src/assets/textures/ms_logo.png");
         Menu menu = new Menu();
         menu.setIconImage(icon);
     }
